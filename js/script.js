3afd7e4a084e97e8edbcb6b35802a45916ab5bb9
Dev <dvptl68@gmail.com>
2020-04-27 16:12:14 -0400

    Add GitHub button animation

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ let g = document.getElementById('github');
 
 let hover = function() {
   g.style.backgroundColor = 'crimson';
+  g.src = 'images/gray-logo.png';
 };
 
 let unhover = function() {
   g.style.backgroundColor = '#B0B0B0';
+  g.src = 'images/crimson-logo.png';
 };
 
 g.addEventListener('mouseenter', hover);
